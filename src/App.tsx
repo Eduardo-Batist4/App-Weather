116e3797c03a308a356cd8fdfddc218d12e38757
Eduardo-Oreia <carloswe3st@gmail.com>
2024-04-05 22:55:44 -0300

    requisição clima e imagens de clima

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { DetailsWather } from "./components/DetailsWather"
 import { InfoWather } from "./components/InfoWather"
 import { SearchWather } from "./components/SearchWather"
 
-
+import rain from "./assets/rain.png";
+import snow from "./assets/snow.png";
+import mist from "./assets/mist.png";
+import cloud from "./assets/cloud.png";
+import clear from "./assets/clear.png";
 
 function App() {
   const [humidity, setHumidity] = useState<number | null>(null)
   const [wind, setWind] = useState<number | null>(null)
   const [temperature, setTemperature] = useState<number | null>(null)
   const [description, setDescription] = useState('')
+  const [chuva, setChuva] = useState('')
 
-  const fetchData = async () => {
+  
+  
+  const fetchData = async (inputValue: string) => {
     try {
-      const url = `https://api.openweathermap.org/data/2.5/weather?q=london&appid=${process.env.API_KEY}`;
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&units=metric&appid=${process.env.API_KEY}`;
+      
       const response = await fetch(url);
   
       if (!response.ok) {
@@ -25,6 +33,9 @@ function App() {
       setDescription(data.weather[0].description);
       setHumidity(data.main.humidity);
       setWind(data.wind.speed);
+
+      changeImg(data.weather[0].main)
+
       console.log(data);
       return data;
     } catch (error) {
@@ -32,20 +43,41 @@ function App() {
       return null;
     }
   };
-  
-  useEffect(() => {
-    fetchData()
-  }, []);
 
   const handleClick = (value: string) => {
-    console.log(value)
+    fetchData(value);
+  }
+
+  const changeImg = (image: string) => {
+    switch (image){
+      case 'Clouds':
+        setChuva(cloud)
+        break
+      case 'Snow':
+        setChuva(snow)
+        break 
+      case 'Rain':
+        setChuva(rain)
+        break 
+      case 'Clear':
+        setChuva(clear)
+        break 
+      case 'Mist':
+        setChuva(mist)
+        break 
+      case 'Haze':
+        setChuva(mist)
+        break 
+      default:
+        setChuva(cloud)
+    }
   }
 
   return (
     <>
     <main className="wather-container"> 
       <SearchWather onSearch={handleClick} />
-      <InfoWather description={description} temperature={temperature} />
+      <InfoWather img={chuva} description={description} temperature={temperature} />
       <DetailsWather humidity={humidity} wind={wind} />
     </main>
     </>
--- src/components/InfoWather.tsx
+++ src/components/InfoWather.tsx
@@ -1,14 +1,14 @@
-import sun from "../assets/clear.png"
 
 interface InfoWather {
     temperature: number | null;
     description: string;
+    img: string;
 }
 
 export const InfoWather = (props: InfoWather) => {
     return (
         <div className="info-wather-container">
-            <img src={sun} alt="img-wather"/>
+            <img src={props.img} alt="img-wather"/>
             <h3 className="temperature-wather">{props.temperature}<span className="celsius">c</span></h3>
             <p className="description-wather">{props.description}</p>
        </div>
